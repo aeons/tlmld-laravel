bd5f0bdba314beec4eeeac7f240b5b845c61bc4a
Bjørn Madsen <bm@aeons.dk>
2015-06-17 13:01:15 +0200

    Add bootstrap and theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+var paths = {
+    'bootstrap': './vendor/bower_components/bootstrap-sass/assets/',
+    'jquery': './vendor/bower_components/jquery/dist/'
+};
 
 elixir(function (mix) {
-    mix.sass('admin/admin.scss');
+    mix.sass('admin/admin.scss',
+        elixir.config.cssOutput + '/admin',
+        {includePaths: [paths.bootstrap + 'stylesheets/']})
+        .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+        .scripts([
+            paths.jquery + 'jquery.js',
+            paths.bootstrap + 'javascripts/bootstrap.js'
+        ], 'public/js/app.js', './');
 });
